7e940e4ca9472a4dafb83cfce14e8c9dc5985a94
Planora <planora@94xx.eu.cc>
2026-09-03 16:25:21 +0800

    Publish CloverPlan Android 0.2.0

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'eu.cloverplan.app',
-  appName: 'Planora',
+  appName: 'CloverPlan',
   webDir: 'www',
   server: { url: 'https://cloverplan.94xx.eu.cc/', cleartext: false, allowNavigation: ['cloverplan.94xx.eu.cc'] },
 };
